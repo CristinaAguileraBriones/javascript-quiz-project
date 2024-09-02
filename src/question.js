336eb00817a77e2ecc60709bf91f6f581cdfcf56
Núria Soley <nuria.soley.93@gmail.com>
2024-09-02 17:52:08 +0200

    Solved mini-projectDay1

--- src/question.js
+++ src/question.js
@@ -7,17 +7,17 @@ class Question {
     }
 
     shuffleChoices() {
-        // Recorremos el array desde el final hasta el principio
+        // Recorremos el this.choices desde el final hasta el principio
     for (let i = this.choices.length - 1; i > 0; i--) {
         // Seleccionamos un índice aleatorio entre 0 y i
         const j = Math.floor(Math.random() * (i + 1));
-        // Intercambiamos los elementos array[i] y array[j]
+        // Intercambiamos los elementos this.choices[i] y this.choices[j]
         [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
     }
     return this.choices;
     }
 }
 
-let obj1 = new Question("Esto es una pregunta", ["A", "B", "C", "D"], "Respuesta correcta", 1)
+let obj1 = new Question("Esto es una pregunta", ["A", "B", "C", "D"], "C", 1)
 
 console.log(obj1.shuffleChoices())
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,44 @@
 class Quiz {
     // YOUR CODE HERE:
     //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
 
-    // 2. getQuestion()
+    getQuestion(){
+        return this.questions[this.currentQuestionIndex]
+    }
     
-    // 3. moveToNextQuestion()
+    moveToNextQuestion(){
+        this.currentQuestionIndex ++
+    }
 
-    // 4. shuffleQuestions()
+    shuffleQuestions(){
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            // Seleccionamos un índice aleatorio entre 0 y i
+            const j = Math.floor(Math.random() * (i + 1));
+            // Intercambiamos los elementos this.questions[i] y this.questions[j]
+            [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+        }
+        return this.questions;
+    }
 
-    // 5. checkAnswer(answer)
+    checkAnswer(answerSelected){ 
+        let currentQuestion = this.getQuestion()
+        if (currentQuestion.answer === answerSelected){
+           this.correctAnswers++
+        }
+    }
 
-    // 6. hasEnded()
+    hasEnded(){
+        if (this.currentQuestionIndex < this.questions.length){
+            return false
+        }else if (this.currentQuestionIndex === this.questions.length){
+            return true
+        }
+    }
 }
