050c6322e3a435059b52c46bda3f65e21d2f5560
Loreto <loreto.garde@gmail.com>
2024-09-02 16:56:47 +0200

    loreto code

--- src/question.js
+++ src/question.js
@@ -1,7 +1,23 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+    constructor(text, choices, answer, difficulty) {
+        this.text = text;
+        this.choices = choices;
+        this.answer = answer;
+        this.difficulty = difficulty;
+    }
 
-    // 2. shuffleChoices()
+    shuffleChoices() {
+        // Recorremos el array desde el final hasta el principio
+    for (let i = this.choices.length - 1; i > 0; i--) {
+        // Seleccionamos un índice aleatorio entre 0 y i
+        const j = Math.floor(Math.random() * (i + 1));
+        // Intercambiamos los elementos array[i] y array[j]
+        [this.choices[i], this.choices[j]] = [this.choices[j], this.choices[i]];
+    }
+    return this.choices;
+    }
 }
+
+let obj1 = new Question("Esto es una pregunta", ["A", "B", "C", "D"], "Respuesta correcta", 1)
+
+console.log(obj1.shuffleChoices())
